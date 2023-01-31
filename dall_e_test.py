cb41dc11ac55c2053e26ed21b99af2f34d6682e5
kozanakyel <kozanakyel@gmail.com>
2023-02-01 00:09:25 +0300

    added dall-e image generation examples

---
+++ dall_e_test.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+import openai
+import requests
+import shutil
+
+load_dotenv()
+
+CHATGPT_ENV=os.getenv('CHATGPT')
+
+openai.api_key = CHATGPT_ENV
+
+response = openai.Image.create(
+  prompt="a blue adidas shoes with different colors lace",
+  n=1,
+  size="256x256"
+)
+image_url = response['data'][0]['url']
+
+file_name = 'images/dalle_image_shoes.png'
+
+res = requests.get(image_url, stream = True)
+
+if res.status_code == 200:
+    with open(file_name,'wb') as f:
+        shutil.copyfileobj(res.raw, f)
+    print('Image sucessfully Downloaded: ', file_name)
+else:
+    print('Image Couldn\'t be retrieved')
+print(image_url)
