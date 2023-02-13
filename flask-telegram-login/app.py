8282d2d8cf680289835632c208e086d32192cc1a
kozanakyel <kozanakyel@gmail.com>
2023-02-13 22:30:22 +0300

    added telegram auth required ecorator

--- flask-telegram-login/app.py
+++ flask-telegram-login/app.py
@@ -5,6 +5,7 @@ import hmac
 import base64
 from dotenv import load_dotenv
 import os
+from functools import wraps
 
 load_dotenv()
 
@@ -16,20 +17,37 @@ app.config.from_object('config')
 app.secret_key = app.config['SECRET_KEY']
 
 
+def telegram_auth_required(func):
+	def wrapper(*args, **kwargs):
+		if 'telegram_data' in session:
+			tg_data = session.get('telegram_data', {})
+		else:
+			tg_data = {
+				"id" : request.args.get('id',None),
+				"first_name" : request.args.get('first_name',None),
+				"last_name" : request.args.get('last_name', None),
+				"username" : request.args.get('username', None),
+				"photo_url" : request.args.get('photo_url', None),
+				"auth_date":  request.args.get('auth_date', None),
+				"hash" : request.args.get('hash', None)
+			}
+		check_user_hash, _ = check_hmac_for_user_auth(telegram_data=tg_data, login_bot_token=LOGIN_BOT_TOKEN)
+		if not check_user_hash:
+			return jsonify({'error': 'unauthorized'})
 
+		#session['telegram_data'] = tg_data
+		return func(*args, **kwargs)
+
+	return wrapper
 
 @app.route('/')
 def index():
 	data = {'bot_name': app.config['BOT_NAME'], 'bot_domain': app.config['BOT_DOMAIN']}
-	print(f'data for bot: {data}')
-	return render_template('index.html',data = data)
-
-@app.route('/dashboard')
-def dashboard():
-	return render_template('dashboard.html')
+	#print(f'data for bot: {data}')
+	return render_template('index.html', data = data)
 
 def string_generator(data_incoming):
-	print(f'data incoming string genarator: {data_incoming}')
+	#print(f'data incoming string genarator: {data_incoming}')
 	data = data_incoming.copy()
 	del data['hash']
 	keys = sorted(data.keys())
@@ -37,10 +55,24 @@ def string_generator(data_incoming):
 	for key in keys:
 		print(f"key: {key}")
 		if data[key] != None:
-			#data[key] = ''
 			string_arr.append(key+'='+data[key])
 	string_cat = '\n'.join(string_arr)
 	return string_cat
+
+def check_hmac_for_user_auth(telegram_data: dict, login_bot_token: str) -> bool:
+	data_check_string = string_generator(telegram_data)
+	secret_key = hashlib.sha256(login_bot_token.encode('utf-8')).digest()
+	secret_key_bytes = secret_key
+	data_check_string_bytes = bytes(data_check_string, 'utf-8')
+	hmac_string = hmac.new(secret_key_bytes, data_check_string_bytes, hashlib.sha256).hexdigest()	
+	return hmac_string == telegram_data["hash"], hmac_string
+
+@app.route('/dashboard')
+@telegram_auth_required
+def dashboard():
+    telegram_data = session.get('telegram_data', {})
+    #print(telegram_data)
+    return render_template('dashboard.html', telegram_data=telegram_data)
 
 @app.route('/login')
 def login():
@@ -53,13 +85,11 @@ def login():
 		"auth_date":  request.args.get('auth_date', None),
 		"hash" : request.args.get('hash', None)
 	}
-	print(f'data login in first: {tg_data}')
-	data_check_string = string_generator(tg_data)
-	secret_key = hashlib.sha256(LOGIN_BOT_TOKEN.encode('utf-8')).digest()
-	secret_key_bytes = secret_key
-	data_check_string_bytes = bytes(data_check_string,'utf-8')
-	hmac_string = hmac.new(secret_key_bytes, data_check_string_bytes, hashlib.sha256).hexdigest()
-	if hmac_string == tg_data['hash']:
+	#print(f'data login in first: {tg_data}')
+ 
+	check_user_hash, hmac_string = check_hmac_for_user_auth(telegram_data=tg_data, login_bot_token=LOGIN_BOT_TOKEN)
+	if check_user_hash:
+		session['telegram_data'] = tg_data
 		return redirect('/dashboard')
 	
 	return jsonify({
@@ -67,6 +97,12 @@ def login():
 				'tg_hash': tg_data['hash'],
 				'tg_data': tg_data
 	})
+ 
+@app.route('/logout')
+def logout():
+    if 'telegram_data' in session:  
+        session.pop('telegram_data', None)
+    return redirect('/')
 
 
 if __name__ == '__main__':
