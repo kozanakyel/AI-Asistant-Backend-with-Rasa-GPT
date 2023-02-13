230182b9cc65c9a0f16f50976372e129ac86d8cf
kozanakyel <kozanakyel@gmail.com>
2023-02-13 21:16:11 +0300

    sepeartaed from modules each other

--- flask-telegram-login/config.py
+++ flask-telegram-login/config.py
@@ -1,3 +1,3 @@
 SECRET_KEY="A very long secret key"
 BOT_NAME="login_user_bot"
-BOT_DOMAIN="https://2e13-95-12-119-215.eu.ngrok.io/login" #for testing 
+BOT_DOMAIN="https://3455-95-12-119-215.eu.ngrok.io/login" #for testing 
