46d8650c330617662ff9c6e4e6b123e5390aaaee
kozanakyel <kozanakyel@gmail.com>
2023-02-18 19:58:36 +0300

    for Login scene added only playfab login component

--- flask-telegram-login/config.py
+++ flask-telegram-login/config.py
@@ -1,3 +1,3 @@
 SECRET_KEY="A very long secret key"
 BOT_NAME="login_user_bot"
-BOT_DOMAIN="https://35a6-95-12-126-82.eu.ngrok.io" #for testing 
+BOT_DOMAIN="https://8b2c-95-12-126-82.eu.ngrok.io" #for testing 
