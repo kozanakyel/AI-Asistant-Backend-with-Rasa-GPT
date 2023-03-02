23f6bd65f7371b35ddb188ad47700fc61a72298d
kozanakyel <kozanakyel@gmail.com>
2023-03-02 23:16:28 +0300

    added transfer nft and jettonsd examples No work because neeed a ton wallet address

--- rasa_chatbot/ton_rasa_connection/ton_wallet_msg.py
+++ rasa_chatbot/ton_rasa_connection/ton_wallet_msg.py
@@ -1,7 +1,9 @@
 from tonsdk.contract.wallet import WalletVersionEnum, Wallets
 from tonsdk.utils import bytes_to_b64str
 from tonsdk.crypto import mnemonic_new
-
+from tonsdk.contract.token.nft import NFTItem
+from tonsdk.contract.token.ft import JettonWallet
+from tonsdk.utils import Address, to_nano
 
 wallet_workchain = 0
 wallet_version = WalletVersionEnum.v3r2
@@ -24,3 +26,39 @@ Base64boc to deploy the wallet: {}
            wallet.address.to_string(),
            wallet.address.to_string(True, True, True),
            base64_boc))
+
+
+############# 
+"""
+Transfer NFT & Jettons
+by creating a transfer message from an owner wallet
+"""
+
+body = NFTItem().create_transfer_body(
+    Address("New Owner Address")
+)
+query = wallet.create_transfer_message(
+    "NFT Item Address",
+    to_nano(0.05, "ton"),
+    0,  # owner wallet seqno
+    payload=body
+)
+nft_boc = bytes_to_b64str(query["message"].to_boc(False))
+
+body = JettonWallet().create_transfer_body(
+    Address("Destination address"),
+    to_nano(40000, "ton")  # jettons amount
+)
+query = wallet.create_transfer_message(
+    "Jetton Wallet Address",
+    to_nano(0.05, "ton"),
+    0,  # owner wallet seqno
+    payload=body
+)
+jettons_boc = bytes_to_b64str(query["message"].to_boc(False))
+
+print("""
+Base64boc to transfer the NFT item: {}
+
+Base64boc to transfer the jettons: {}
+""".format(nft_boc, jettons_boc))
